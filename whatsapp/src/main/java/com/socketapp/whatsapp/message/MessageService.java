aab86b69a9b6b28a464b303f8f188cef83b567d3
NQTrang1801 <trangnguyen180103@gmail.com>
2025-02-10 10:15:21 +0700

    implemented the notification service

--- whatsapp/src/main/java/com/socketapp/whatsapp/message/MessageService.java
+++ whatsapp/src/main/java/com/socketapp/whatsapp/message/MessageService.java
@@ -3,6 +3,10 @@ package com.socketapp.whatsapp.message;
 import com.socketapp.whatsapp.chat.Chat;
 import com.socketapp.whatsapp.chat.ChatRepository;
 import com.socketapp.whatsapp.file.FileService;
+import com.socketapp.whatsapp.file.FileUtils;
+import com.socketapp.whatsapp.notification.Notification;
+import com.socketapp.whatsapp.notification.NotificationService;
+import com.socketapp.whatsapp.notification.NotificationType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -19,10 +23,11 @@ public class MessageService {
     private final ChatRepository chatRepository;
     private final MessageMapper mapper;
     private final FileService fileService;
+    private final NotificationService notificationService;
 
     public void saveMessage(MessageRequest messageRequest){
         Chat chat = chatRepository.findById(messageRequest.getChatId())
-                .orElseThrow(() -> new IllegalArgumentException("Chat not found"));
+                .orElseThrow(() -> new IllegalArgumentException("Chat was not found"));
 
         Message message = new Message();
         message.setContent(messageRequest.getContent());
@@ -34,7 +39,17 @@ public class MessageService {
 
         messageRepository.save(message);
 
-        //todo notification
+        Notification notification = Notification.builder()
+                .chatId(chat.getId())
+                .messageType(messageRequest.getType())
+                .content(messageRequest.getContent())
+                .senderId(messageRequest.getSenderId())
+                .receiverId(messageRequest.getReceiverId())
+                .type(NotificationType.MESSAGE)
+                .chatName(chat.getChatName(message.getSenderId()))
+                .build();
+
+        notificationService.sendNotification(message.getReceiverId(), notification);
     }
 
     public List<MessageResponse> findChatMessages(String chatId) {
@@ -47,17 +62,24 @@ public class MessageService {
     @Transactional
     public void setMessageToSeen(String chatId, Authentication authentication) {
         Chat chat = chatRepository.findById(chatId)
-                .orElseThrow(() -> new IllegalArgumentException("Chat not found"));
+                .orElseThrow(() -> new IllegalArgumentException("Chat was not found"));
 
-//        final String recipientId = getRecipientId(chat, authentication);
+        final String recipientId = getRecipientId(chat, authentication);
         messageRepository.setMessagesToSeenByChat(chatId, MessageState.SEEN);
 
-        // todo notification
+        Notification notification = Notification.builder()
+                .chatId(chat.getId())
+                .type(NotificationType.SEEN)
+                .receiverId(recipientId)
+                .senderId(getSenderId(chat, authentication))
+                .build();
+
+        notificationService.sendNotification(recipientId, notification);
     }
 
     public void uploadMediaMessage(String chatId, MultipartFile file, Authentication authentication) {
         Chat chat = chatRepository.findById(chatId)
-                .orElseThrow(() -> new IllegalArgumentException("Chat not found"));
+                .orElseThrow(() -> new IllegalArgumentException("Chat was not found"));
 
         final String senderId = getSenderId(chat, authentication);
         final String recipientId = getRecipientId(chat, authentication);
@@ -71,8 +93,17 @@ public class MessageService {
         message.setState(MessageState.SENT);
         message.setMediaFilePath(filePath);
         messageRepository.save(message);
-        
-        // todo notification
+
+        Notification notification = Notification.builder()
+                .chatId(chat.getId())
+                .type(NotificationType.IMAGE)
+                .messageType(MessageType.IMAGE)
+                .senderId(senderId)
+                .receiverId(recipientId)
+                .media(FileUtils.readFileFromLocation(filePath))
+                .build();
+
+        notificationService.sendNotification(recipientId, notification);
     }
 
     private String getSenderId(Chat chat, Authentication authentication) {
---
+++ whatsapp/src/main/java/com/socketapp/whatsapp/notification/NotificationType.java
@@ -0,0 +1,9 @@
+package com.socketapp.whatsapp.notification;
+
+public enum NotificationType {
+    SEEN,
+    MESSAGE,
+    IMAGE,
+    AUDIO,
+    VIDEO,
+}
