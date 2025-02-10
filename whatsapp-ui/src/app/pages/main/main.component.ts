f975dd6a2bf5e671c948f2b7267a97d7a3c12ced
NQTrang1801 <trangnguyen180103@gmail.com>
2025-02-10 16:29:58 +0700

    ui:implemented the chat list component

---
+++ whatsapp-ui/src/app/pages/main/main.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {ChatListComponent} from '../../components/chat-list/chat-list.component';
+import {ChatResponse} from '../../services/models/chat-response';
+import {ChatService} from '../../services/services';
+import {KeycloakService} from '../../utils/keycloak/keycloak.service';
+
+@Component({
+  selector: 'app-main',
+  imports: [
+    ChatListComponent
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent implements OnInit {
+
+  chats: Array<ChatResponse> = [];
+  constructor(
+    private chatService: ChatService,
+    private keycloackService: KeycloakService
+  ) {
+  }
+
+  ngOnInit() {
+    this.getAllChats();
+  }
+
+  private getAllChats() {
+    this.chatService.getChatsByReceiver()
+      .subscribe({
+        next: (res) => {
+          this.chats = res;
+        }
+      })
+  }
+
+  logout() {
+    this.keycloackService.logout();
+  }
+
+  userProfile() {
+    this.keycloackService.accountManagement();
+  }
+}
