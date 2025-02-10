52944f16c1180eebc7f92dfc2e11c233931e277d
NQTrang1801 <trangnguyen180103@gmail.com>
2025-02-10 17:26:38 +0700

    ui:implemented the messages list

--- whatsapp-ui/src/app/pages/main/main.component.ts
+++ whatsapp-ui/src/app/pages/main/main.component.ts
@@ -1,13 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {ChatListComponent} from '../../components/chat-list/chat-list.component';
 import {ChatResponse} from '../../services/models/chat-response';
-import {ChatService} from '../../services/services';
+import {ChatService, MessageService} from '../../services/services';
 import {KeycloakService} from '../../utils/keycloak/keycloak.service';
+import {MessageResponse} from '../../services/models/message-response';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-main',
   imports: [
-    ChatListComponent
+    ChatListComponent,
+    DatePipe
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
@@ -15,9 +18,13 @@ import {KeycloakService} from '../../utils/keycloak/keycloak.service';
 export class MainComponent implements OnInit {
 
   chats: Array<ChatResponse> = [];
+  selectedChat: ChatResponse = {};
+  chatMessages: MessageResponse[] = [];
+
   constructor(
     private chatService: ChatService,
-    private keycloackService: KeycloakService
+    private keycloackService: KeycloakService,
+    private messageService: MessageService
   ) {
   }
 
@@ -40,5 +47,30 @@ export class MainComponent implements OnInit {
 
   userProfile() {
     this.keycloackService.accountManagement();
+  }
+
+  chatSelected(chatResponse: ChatResponse) {
+    this.selectedChat = chatResponse;
+    this.getAllChatMessages(chatResponse.id as string);
+    this.setMessagesToSeen();
+    // this.selectedChat.unreadCount = 0;
+  }
+
+  private getAllChatMessages(chatId: string) {
+    this.messageService.getMessages({
+      'chat-id': chatId
+    }).subscribe({
+      next: (messages) => {
+        this.chatMessages = messages;
+      }
+    })
+  }
+
+  private setMessagesToSeen() {
+
+  }
+
+  isSelfMessage(message: MessageResponse) {
+    return message.senderId === this.keycloackService.userId;
   }
 }
